1a5508de906b643919bcf4c495f7d306fc6b9732
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-08-11 13:01:50 +0200

    changed title to aria-label

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -4,7 +4,7 @@ import './Select.css';
 const Select = ({ options, handleChange, title }) => {
   return (
     <div className="dropdown__box">
-      <select title={title} className="dropdown__menu" onChange={(e) => handleChange(e.target.value)}>
+      <select aria-label={title} className="dropdown__menu" onChange={(e) => handleChange(e.target.value)}>
         {options.map((option, i) => {
           return (
             <option key={i} className="dropdown__item" value={option.value}>
